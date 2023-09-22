6719c61dc13996b64ddf44ab7c9e816e894683a5
Huynh-Duy-Quy <huynhduyquy2001@gmail.com>
2023-09-23 06:44:19 +0700

    23-09-2023 06:44

--- src/main/java/com/viesonet/dao/ShoppingCartDao.java
+++ src/main/java/com/viesonet/dao/ShoppingCartDao.java
@@ -1,9 +1,14 @@
 package com.viesonet.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.viesonet.entity.Products;
 import com.viesonet.entity.ShoppingCart;
+import com.viesonet.entity.Users;
 
 public interface ShoppingCartDao extends JpaRepository<ShoppingCart, String> {
+    @Query("SELECT obj FROM ShoppingCart obj WHERE obj.product.productId = :productId AND obj.user.userId=:userId AND obj.color=:color")
+    public ShoppingCart findCartByProductId(String userId, int productId, String color);
 
 }
