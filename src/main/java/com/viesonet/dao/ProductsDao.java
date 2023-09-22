19ae2968ad97303c600d9a1ac6ef8130e55fb269
Vie_Hiu <daynewtran@gmail.com>
2023-09-22 16:54:45 +0700

    22/09/2023 chưa xong dì hết

--- src/main/java/com/viesonet/dao/ProductsDao.java
+++ src/main/java/com/viesonet/dao/ProductsDao.java
@@ -9,8 +9,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductsDao extends JpaRepository<Products, Integer> {
+    @Query("SELECT p FROM Products p " +
+            "INNER JOIN p.favoriteProducts f " +
+            "WHERE f.user.userId = :userId")
+    List<Products> findFavoriteProductsByUserId(String userId);
+
     @Query("SELECT p FROM Products p WHERE p.user.userId IN :userId AND p.productStatus.statusId=1")
     List<Products> getShopping(List<String> userId);
 
