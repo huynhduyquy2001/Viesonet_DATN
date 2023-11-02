44138c023508e3074053f410797fe449123081d3
Vyx-stack <133791259+Vyx-stack@users.noreply.github.com>
2023-11-02 16:40:55 +0700

    4:40 | 2 - 10 - 2023

--- src/main/java/com/viesonet/security/AuthConfig.java
+++ src/main/java/com/viesonet/security/AuthConfig.java
@@ -13,16 +13,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import com.viesonet.entity.Accounts;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -31,15 +25,13 @@ import com.viesonet.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableMethodSecurity
-// (securedEnabled = true,
-// jsr250Enabled = true,
-// prePostEnabled = true) // by default
-public class AuthConfig { // extends WebSecurityConfigurerAdapter {
+
+public class AuthConfig {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
 
     @Autowired
-    private JwtAuthenticationEntryPoint unauthorizedHandler;
+    private JwtAuthenticationEntryPoint JwtAuthenticationEntryPoint;
 
     @Autowired
     AccountsService accountsService;
@@ -49,12 +41,6 @@ public class AuthConfig { // extends WebSecurityConfigurerAdapter {
         return new JwtAuthenticationTokenFilter();
     }
 
-    // @Override
-    // public void configure(AuthenticationManagerBuilder
-    // authenticationManagerBuilder) throws Exception {
-    // authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    // }
-
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -63,12 +49,6 @@ public class AuthConfig { // extends WebSecurityConfigurerAdapter {
 
         return authProvider;
     }
-
-    // @Bean
-    // @Override
-    // public AuthenticationManager authenticationManagerBean() throws Exception {
-    // return super.authenticationManagerBean();
-    // }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
@@ -92,6 +72,7 @@ public class AuthConfig { // extends WebSecurityConfigurerAdapter {
                 // .requestMatchers("/**").hasAnyRole("1", "2", "3")
                 // .anyRequest()
                 .permitAll()
+                .and().exceptionHandling().authenticationEntryPoint(JwtAuthenticationEntryPoint)
                 .and()
                 .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(management -> management
--- src/main/java/com/viesonet/security/JwtTokenUtil.java
+++ src/main/java/com/viesonet/security/JwtTokenUtil.java
@@ -2,6 +2,7 @@ package com.viesonet.security;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +10,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
@@ -24,6 +28,9 @@ public class JwtTokenUtil {
     @Value("${bezkoder.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
+    @Value("${bezkoder.app.jwtRefreshExpirationMs}")
+    private int refreshExpirationDateInMs;
+
     public String generateJwtToken(Authentication authentication) {
 
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
@@ -37,33 +44,50 @@ public class JwtTokenUtil {
     }
 
     private Key key() {
-        // ecode chuỗi jwtSecret
+        // Decode chuỗi jwtSecret
         byte[] decodedSecret = Decoders.BASE64.decode(jwtSecret);
 
         // Encode lại chuỗi jwtSecret
         String encodedSecret = Encoders.BASE64.encode(decodedSecret);
-
-        System.out.println("Decoded Secret: " + new String(decodedSecret));
-        System.out.println("Encoded Secret: " + encodedSecret);
-
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
     public String getUserNameFromJwtToken(String token) {
-        System.out.println("Da xac thuc roi");
         return Jwts.parserBuilder().setSigningKey(key()).build()
                 .parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateJwtToken(String authToken) {
         try {
-            System.out.println("key: " + key());
-            System.out.println("authToken: " + authToken);
             Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
             return true;
         } catch (JwtException e) {
             logger.error("JWT token validation failed: {}", e.getMessage());
             return false;
+        }
+    }
+
+    public String doGenerateRefreshToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpirationDateInMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+
+    }
+
+    public boolean isJwtTokenExpired(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(jwtSecret) // Thay yourSecretKey bằng khóa bí mật của bạn
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            // Lấy thời gian hết hạn từ JWT và so sánh với thời gian hiện tại
+            long expiration = claims.getExpiration().getTime();
+            return expiration < System.currentTimeMillis();
+        } catch (Exception e) {
+            // Xảy ra lỗi khi phân tích JWT, có thể là JWT không hợp lệ hoặc đã hết hạn
+            return true;
         }
     }
 
